422d3ffcb854809c1609d2f8240664de9b349336
Nicolas Choquet <nchoquet@norsys.fr>
2023-10-18 18:12:46 +0200

    - Ajout du contenu du script d'installation - Ajout d'un script de desinstallation - Ajout de doc dans le README.md - Ajout du fichier de desinstallation dans l'asset de release

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # json2bash
 Petit binaire en golang pour transformer un tableau ou un objet json en tableau associatif bash
+
+# Installation
+
+```shell
+SUDO_PASSWD=tonmotdepasse bash ./install.sh
+```
+
+# Désinstallation
+
+```shell
+SUDO_PASSWD=tonmotdepasse bash ./uninstall.sh
+```
+
+# Utilisation
+
+```shell
+json2bash -json[=]"content json"
+# ou
+json2bash -json[=]"url du fichier json"
+# ou
+json2bash -json[=]"chemin local du fichier json via le protocol file://"
+```
--- uninstall.sh
+++ uninstall.sh
@@ -9,30 +9,22 @@ echo "| \$\$  | \$\$ \____  \$\$| \$\$  | \$\$| \$\$  | \$\$| \$\$      | \$\$  
 echo "|  \$\$\$\$\$\$/ /\$\$\$\$\$\$\$/|  \$\$\$\$\$\$/| \$\$  | \$\$| \$\$\$\$\$\$\$\$| \$\$\$\$\$\$\$/|  \$\$\$\$\$\$\$ /\$\$\$\$\$\$\$/| \$\$  | \$\$"
 echo " \______/ |_______/  \______/ |__/  |__/|________/|_______/  \_______/|_______/ |__/  |__/"
 echo "                                                                                    "
-echo "        ___         _        _ _      _   _          "
-echo "       |_ _|_ _  __| |_ __ _| | |__ _| |_(_)___ _ _  "
-echo "        | || ' \(_-<  _/ _' | | / _' |  _| / _ \ ' \ "
-echo "       |___|_||_/__/\__\__,_|_|_\__,_|\__|_\___/_||_|"
-echo "                                                     "
+echo "        ___         _         _        _ _      _   _           "
+echo "       |   \ ___ __(_)_ _  __| |_ __ _| | |__ _| |_(_)___ _ _   "
+echo "        | |) / -_|_-< | ' \(_-<  _/ _' | | / _' |  _| / _ \ ' \ "
+echo "       |___/\___/__/_|_||_/__/\__\__,_|_|_\__,_|\__|_\___/_||_| "
+echo "                                                                "
 
 if [[ "$SUDO_PASSWD" == "" ]];then
   echo "❌ Saisissez votre mot de passe sudo dans la variable d'environnement 'SUDO_PASSWD'"
   exit 1;
 fi
 
-if [[ -f /bin/json2bash ]];then
-  echo "❌ Json2Bash à déjà été installé."
+if [[ ! -f /bin/json2bash ]];then
+  echo "❌ Json2Bash n'est pas installé."
   exit 0
 fi
 
-echo "$SUDO_PASSWD" | sudo -S ln -s $(pwd)/json2bash /bin/json2bash
+echo "$SUDO_PASSWD" | sudo -S rm -rf /bin/json2bash
 
-echo "✅ Json2Bash à été installé dans le répertoire /bin"
-echo "➡️ Vous pouvez utiliser le le logiciel comme ceci :"
-echo "  • json2bash -json=\"content json\""
-echo "  • json2bash -json \"content json\""
-echo "  • json2bash -json=\"url du fichier json\""
-echo "  • json2bash -json \"url du fichier json\""
-echo "  • json2bash -json=\"chemin local du fichier json via le protocol file://\""
-echo "  • json2bash -json \"chemin local du fichier json via le protocol file://\""
-
+echo "✅ Json2Bash à été desinstallé."
